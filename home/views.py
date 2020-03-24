168e35a626c6a0400ac0c1c7ff74a23e2caa634d
roylanmartinez <gilbertroylan.martinez@e-campus.uab.cat>
2020-03-24 19:42:32 +0100

    operational home

--- home/views.py
+++ home/views.py
@@ -1,19 +1,22 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import Contactf, Contactfes
-
-# Create your views here.
+from .ms import go
 
 
 def home(request):
     if request.method == 'POST':
         form = Contactf(request.POST or None)
         if form.is_valid():
+            nemail = str(request.POST['email'])
+            nname = str(request.POST['name']).split(' ')
+            nm = ' '.join([word.capitalize() for word in nname])
             form.save()
-            messages.info(request, 'lol', extra_tags='green')
+            go(nombre=nm, correo=nemail)
+            messages.success(request, nm)
             return redirect('pag-en')
         else:
-            messages.info(request, 'lol', extra_tags='red')
+            messages.info(request, '')
             return redirect('pag-en')
 
     else:
@@ -25,11 +28,15 @@ def homees(request):
     if request.method == 'POST':
         form = Contactfes(request.POST or None)
         if form.is_valid():
+            nemail = str(request.POST['email'])
+            nname = str(request.POST['name']).split(' ')
+            nm = ' '.join([word.capitalize() for word in nname])
             form.save()
-            messages.info(request, 'lol', extra_tags='green')
+            go(nombre=nm, correo=nemail)
+            messages.success(request, nm)
             return redirect('pag-es')
         else:
-            messages.info(request, 'lol', extra_tags='red')
+            messages.info(request, '')
             return redirect('pag-es')
 
     else:
